4f8e8960dddd77604b33d6dfb9d76bdc02970477
Administrator <tibianmod@gmail.com>
2020-10-16 17:07:51 +0200

    delete dist

--- dist/types/Model.d.ts
+++
@@ -1,4 +0,0 @@
-import { Model } from '@vuex-orm/core';
-export declare function OrmModel(entity: string, baseEntity?: string, types?: {
-    [key: string]: typeof Model;
-}): <Model_1 extends Function>(constructor: Model_1) => Model_1;
