06b03227741052150128c9d59e8b9bba968c14b5
Administrator <tibianmod@gmail.com>
2020-10-16 17:09:53 +0200

    Generate assets
    fix - #13

---
+++ dist/types/index.d.ts
@@ -0,0 +1,3 @@
+export { OrmModel } from './model';
+export { ORMDatabase } from './database';
+export { AttrField, BelongsToField, BelongsToManyField, BooleanField, Field, HasManyByField, HasManyField, HasManyThroughField, HasOneField, IncrementField, MorphManyField, MorphOneField, MorphToField, MorphToManyField, MorphedByManyField, NumberField, PrimaryKey, StringField, UidField } from './attributes';
