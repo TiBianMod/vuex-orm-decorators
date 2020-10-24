b7f35c70430cecdea647cd17bb7e2d4ab920a806
Administrator <tibianmod@gmail.com>
2020-10-24 14:53:34 +0200

    Remove dist assets

--- dist/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { OrmModel } from './model';
-export { ORMDatabase } from './database';
-export { AttrField, BelongsToField, BelongsToManyField, BooleanField, Field, HasManyByField, HasManyField, HasManyThroughField, HasOneField, IncrementField, MorphManyField, MorphOneField, MorphToField, MorphToManyField, MorphedByManyField, NumberField, PrimaryKey, StringField, UidField } from './attributes';
