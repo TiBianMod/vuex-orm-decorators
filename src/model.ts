f5287978d2d17cc667ea244dcaf92b9d87a879db
Administrator <tibianmod@gmail.com>
2021-01-09 00:51:03 +0100

    Update model src

--- src/model.ts
+++ src/model.ts
@@ -1,8 +1,8 @@
 import { Model } from '@vuex-orm/core';
-import { ORMDatabase } from './database';
+import { ORMDatabase } from '@/database';
 
 export function OrmModel(entity: string, baseEntity?: string, types?: { [key: string]: typeof Model }) {
-    return function <Model extends Function>(constructor: Model): Model {
+    return <Model extends Function>(constructor: Model): Model => {
         // The name that is going be used as module name in Vuex Store.
         (constructor as any).entity = entity;
 
@@ -10,16 +10,6 @@ export function OrmModel(entity: string, baseEntity?: string, types?: { [key: st
         if (baseEntity) {
             (constructor as any).baseEntity = baseEntity;
         }
-
-        // The definition of the fields of the model and its relations.
-        (constructor as any).fields = () => {
-            const fields = (constructor as any)._fields || {};
-
-            return {
-                ...(constructor as any).prototype?._super?.fields(),
-                ...fields
-            };
-        };
 
         // The 'types mapping' used to dispatch entities based on their discriminator field.
         if (types && baseEntity) {
