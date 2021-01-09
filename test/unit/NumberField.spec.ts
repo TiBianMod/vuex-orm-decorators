2b81c3754106e1c9de8fc8947b33f00305b268e4
Administrator <tibianmod@gmail.com>
2021-01-09 01:18:05 +0100

    Add Test for NumberField

---
+++ test/unit/NumberField.spec.ts
@@ -0,0 +1,59 @@
+import { Model, Number } from '@vuex-orm/core';
+import { NumberField } from '@/attributes';
+import { OrmModel } from '@/model';
+
+describe('NumberField', () => {
+    it('can define `number` field', () => {
+        @OrmModel('users')
+        class User extends Model {
+
+            @NumberField() age!: number;
+
+        }
+
+        expect(User.getFields().age).toBeInstanceOf(Number);
+        expect((new User()).age).toBe(0);
+        expect((User.getFields().age as Number).isNullable).toBe(false);
+    });
+
+    it('can define `number` field with default value', () => {
+        @OrmModel('users')
+        class User extends Model {
+
+            @NumberField(25) age!: number;
+
+        }
+
+        expect(User.getFields().age).toBeInstanceOf(Number);
+        expect((new User()).age).toBe(25);
+        expect((User.getFields().age as Number).isNullable).toBe(false);
+    });
+
+    it('can define `number` field with null value', () => {
+        @OrmModel('users')
+        class User extends Model {
+
+            @NumberField(null) age!: number;
+
+        }
+
+        expect(User.getFields().age).toBeInstanceOf(Number);
+        expect((new User()).age).toBe(null);
+        expect((User.getFields().age as Number).isNullable).toBe(true);
+    });
+
+    it('can mutate the given value', () => {
+        @OrmModel('users')
+        class User extends Model {
+
+            @NumberField(25, (value: number) => {
+                return value + 1;
+            }) age!: number;
+
+        }
+
+        expect(User.getFields().age).toBeInstanceOf(Number);
+        expect((new User()).age).toBe(26);
+        expect((User.getFields().age as Number).isNullable).toBe(false);
+    });
+});
