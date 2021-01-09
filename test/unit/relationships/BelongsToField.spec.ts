ae013a42b96ea8d43790851898c957338e971006
Administrator <tibianmod@gmail.com>
2021-01-09 16:10:35 +0100

    Add Test for BelongsToField

---
+++ test/unit/relationships/BelongsToField.spec.ts
@@ -0,0 +1,46 @@
+import { BelongsTo } from '@vuex-orm/core';
+import { ORMDatabase } from '@/database';
+import { Profile } from 'test/unit/relationships/fixtures/Profile';
+import { Store } from 'vuex';
+import { User } from 'test/unit/relationships/fixtures/User';
+
+describe('BelongsToField', () => {
+    let store: Store<any>;
+
+    afterEach(() => {
+        store.$db().entities = [];
+    });
+
+    it('can define the property as a `Belongs To` relationship field', () => {
+        store = new Store({
+            plugins: [ORMDatabase.install()],
+        });
+
+        Profile.insert({
+            data: {
+                id: 1, user_id: 10, age: 25, sex: 'male',
+                user: {
+                    id: 10,
+                    name: 'John Doe',
+                },
+            },
+        });
+
+        const field = Profile.getFields().user as BelongsTo;
+
+        expect(field).toBeInstanceOf(BelongsTo);
+
+        expect(field.parent).toBe(User);
+        expect(field.foreignKey).toBe('user_id');
+        expect(field.ownerKey).toBe('id');
+
+        const profile = Profile.query().with('user').first();
+
+        expect(profile?.user).toEqual({
+            $id: '10', id: 10, name: 'John Doe', profile: null,
+        });
+
+        expect(User.all()).toHaveLength(1);
+        expect(Profile.all()).toHaveLength(1);
+    });
+});
---
+++ test/unit/relationships/fixtures/User.ts
@@ -0,0 +1,15 @@
+import { HasOneField, NumberField, StringField } from '@/attributes';
+import { Model } from '@vuex-orm/core';
+import { OrmModel } from '@/model';
+import { Profile } from 'test/unit/relationships/fixtures/Profile';
+
+@OrmModel('users')
+export class User extends Model {
+
+    @NumberField() id!: number;
+
+    @StringField() name!: string;
+
+    @HasOneField(Profile, 'user_id') profile!: Profile;
+
+}
