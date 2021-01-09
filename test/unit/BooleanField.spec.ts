91c904c700a5285d46217308a2e5704bc411531d
Administrator <tibianmod@gmail.com>
2021-01-09 01:17:55 +0100

    Add Test for BooleanField

---
+++ test/unit/BooleanField.spec.ts
@@ -0,0 +1,59 @@
+import { Boolean, Model } from '@vuex-orm/core';
+import { BooleanField } from '@/attributes';
+import { OrmModel } from '@/model';
+
+describe('BooleanField', () => {
+    it('can define `boolean` field', () => {
+        @OrmModel('users')
+        class User extends Model {
+
+            @BooleanField() active!: boolean;
+        
+        }
+
+        expect(User.getFields().active).toBeInstanceOf(Boolean);
+        expect((new User()).active).toBe(false);
+        expect((User.getFields().active as Boolean).isNullable).toBe(false);
+    });
+
+    it('can define `boolean` field with default value', () => {
+        @OrmModel('users')
+        class User extends Model {
+
+            @BooleanField(true) active!: boolean;
+        
+        }
+
+        expect(User.getFields().active).toBeInstanceOf(Boolean);
+        expect((new User()).active).toBe(true);
+        expect((User.getFields().active as Boolean).isNullable).toBe(false);
+    });
+
+    it('can define `boolean` field with null value', () => {
+        @OrmModel('users')
+        class User extends Model {
+
+            @BooleanField(null) active!: boolean;
+        
+        }
+
+        expect(User.getFields().active).toBeInstanceOf(Boolean);
+        expect((new User()).active).toBe(null);
+        expect((User.getFields().active as Boolean).isNullable).toBe(true);
+    });
+
+    it('can mutate the given value', () => {
+        @OrmModel('users')
+        class User extends Model {
+
+            @BooleanField(false, (value: boolean) => {
+                return ! value;
+            }) active!: boolean;
+        
+        }
+
+        expect(User.getFields().active).toBeInstanceOf(Boolean);
+        expect((new User()).active).toBe(true);
+        expect((User.getFields().active as Boolean).isNullable).toBe(false);
+    });
+});
