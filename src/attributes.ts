b99311e90f4cbe14326505ae2b569628dc0e60d5
Tomáš Kulhánek <jsem@tomaskulhanek.cz>
2021-08-21 01:46:07 +0200

    Fix path and version

--- src/attributes.ts
+++ src/attributes.ts
@@ -1,5 +1,5 @@
 import { Attribute, Model } from '@vuex-orm/core';
-import Mutator from '@vuex-orm/core/lib/attributes/contracts/Mutator';
+import Mutator from '@vuex-orm/core/dist/src/attributes/contracts/Mutator';
 
 /**
  * Sets the property as the primary key of the model
